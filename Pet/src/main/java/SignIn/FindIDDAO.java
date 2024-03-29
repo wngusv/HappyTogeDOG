e816bee1b907bd558c38beb89ce423855d89144e
gayoung <7rmile@gmail.com>
2024-03-29 13:05:13 +0900

    com.google.code.gson 라이브러리 의존성 추가, 아이디찾기 누르고 본인인증 누르면 일단 sms에 인증번호가 문자로 전송되는 것까지 완..ㄹ ㅛ..

---
+++ Pet/src/main/java/SignIn/FindIDDAO.java
@@ -0,0 +1,37 @@
+package SignIn;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Util.MyWebContextListener;
+
+public class FindIDDAO {
+
+	public String findID(String name, String phone) {
+		// 본인인증 숫자랑 일치하는지 확인후 본인인증 번호랑 일치하면 디비로 들어가기
+		
+		
+		
+		
+		
+		
+		
+		String sql = "SELECT id FROM user where user_name = ? and phone = ? ";
+		try (Connection conn = MyWebContextListener.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, name);
+			stmt.setString(2, phone);
+			try (ResultSet rs = stmt.executeQuery()) {
+				if (rs.next()) {
+					String id = rs.getString("id");
+					return id; // id 반환
+				} 
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
