df38ebc8f1825cb11abacc891d8d243b44bef2c2
Kanghyun2 <rkdguse@naver.com>
2024-03-29 11:35:01 +0900

    d

--- Pet/src/main/java/SignUp/SignUpDAO.java
+++ Pet/src/main/java/SignUp/SignUpDAO.java
@@ -2,6 +2,7 @@ package SignUp;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import SignIn.User;
@@ -18,11 +19,29 @@ public class SignUpDAO {
             stmt.setString(4, user.getPhoneNumber());
             stmt.setString(5, user.getAddress());
             stmt.setString(6, user.getAddress_detail());
-            stmt.executeUpdate();
+            
             
             conn.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+    public boolean checkID(String id) {
+        String sql = "SELECT COUNT(*) FROM user WHERE id = ?";
+        try (Connection conn = MyWebContextListener.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, id);
+            System.out.println(id);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                	System.out.println("true");
+                    int count = rs.getInt(1);
+                    return count > 0; // 아이디가 이미 존재하면 true 반환
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false; // 아이디가 존재하지 않음
     }
 }
