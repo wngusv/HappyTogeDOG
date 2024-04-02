4c39e17c44b602a362001a0d64810c93f90521d2
Heo_gom <gjtjdwo1004@naver.com>
2024-04-02 15:22:34 +0900

    푸시..

--- Pet/src/main/java/Util/LocalStrayListUpdateListener.java
+++ Pet/src/main/java/Util/LocalStrayListUpdateListener.java
@@ -57,9 +57,16 @@ public class LocalStrayListUpdateListener implements ServletContextListener {
 
 	private static void updateData() {
 		try {
-			allAnimalList = makeAllAnimalList();
+			if (allAnimalList == null) {
+	            allAnimalList = new ArrayList<>();
+	        }
+			noticeAnimalList = makeNoticeAnimalList();
+			protectAnimalList = makeProtectAnimalList();
+			allAnimalList.addAll(noticeAnimalList);
+			allAnimalList.addAll(protectAnimalList);
+			
 			localGovernmentList = makeLocalGovernmentList();
-			sortAnimalList(allAnimalList);
+//			sortAnimalList(allAnimalList);
 			
 			System.out.println("AllAnimalUpdate");
 		} catch (Exception e) {
@@ -69,21 +76,21 @@ public class LocalStrayListUpdateListener implements ServletContextListener {
 
 
 
-	private static void sortAnimalList(List<Animal> allAnimalList) {
-	    // 오늘 날짜 String 형태로 설정
-	    String todayDateString = LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
+//	private static void sortAnimalList(List<Animal> allAnimalList) {
+//	    // 오늘 날짜 String 형태로 설정
+//	    String todayDateString = LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
+//
+//	    for (Animal animal : allAnimalList) {
+//	        if (animal.getNoticeEdt().compareTo(todayDateString) >= 0) {
+//	            noticeAnimalList.add(animal);  // NoticeEdt가 오늘 날짜보다 크거나 같으면 공고 중 동물 목록에 추가
+//	        } else {
+//	            protectAnimalList.add(animal);  // NoticeEdt가 오늘 날짜보다 작으면 보호 중 동물 목록에 추가
+//	        }
+//	    }
+//	}
 
-	    for (Animal animal : allAnimalList) {
-	        if (animal.getNoticeEdt().compareTo(todayDateString) >= 0) {
-	            noticeAnimalList.add(animal);  // NoticeEdt가 오늘 날짜보다 크거나 같으면 공고 중 동물 목록에 추가
-	        } else {
-	            protectAnimalList.add(animal);  // NoticeEdt가 오늘 날짜보다 작으면 보호 중 동물 목록에 추가
-	        }
-	    }
-	}
 
-
-	public static List<Animal> makeAllAnimalList()
+	public static List<Animal> makeNoticeAnimalList()
 			throws UnsupportedEncodingException, MalformedURLException, IOException, ProtocolException {
 		StringBuilder urlBuilder = new StringBuilder(
 				"http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic"); /* URL */
@@ -95,7 +102,46 @@ public class LocalStrayListUpdateListener implements ServletContextListener {
 //		appendItem(urlBuilder,"kind"," "); /* 품종코드 (품종 조회 OPEN API 참조) */
 //		appendItem(urlBuilder,"org_cd"," "); /* 시군구코드 (시군구 조회 OPEN API 참조) */
 //		appendItem(urlBuilder,"care_reg_no"," "); /* 보호소번호 (보호소 조회 OPEN API 참조) */
-//		appendItem(urlBuilder,"state"," "); /* 상태(전체 : null(빈값), 공고중 : notice, 보호중 : protect) */
+		appendItem(urlBuilder,"state","notice"); /* 상태(전체 : null(빈값), 공고중 : notice, 보호중 : protect) */
+//		appendItem(urlBuilder,"neuter_yn"," "); /* 상태 (전체 : null(빈값), 예 : Y, 아니오 : N, 미상 : U) */
+		appendItem(urlBuilder, "pageNo", "1"); // 페이지 번호 (기본값 : 1)
+		appendItem(urlBuilder, "numOfRows", "1000"); // 페이지당 보여줄 개수 (1,000 이하), 기본값 : 10
+		appendItem(urlBuilder, "_type", "json"); // xml(기본값) 또는 json
+		URL url = new URL(urlBuilder.toString());
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Content-type", "application/json");
+		System.out.println("Response code: " + conn.getResponseCode());
+		BufferedReader rd;
+		if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+			rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		} else {
+			rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+		}
+		StringBuilder jsonData = new StringBuilder();
+		String line;
+		while ((line = rd.readLine()) != null) {
+			jsonData.append(line);
+		}
+		rd.close();
+		conn.disconnect();
+
+		List<Animal> animalList = AnimalResponseParser.parseJsonResponse(jsonData.toString());
+		return animalList;
+	}
+	public static List<Animal> makeProtectAnimalList()
+			throws UnsupportedEncodingException, MalformedURLException, IOException, ProtocolException {
+		StringBuilder urlBuilder = new StringBuilder(
+				"http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic"); /* URL */
+		urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + API_KEY); /* Service Key */
+		appendItem(urlBuilder, "upr_cd", BUSAN_CODE); // 시도코드 (시도 조회 OPEN API 참조)
+//		appendItem(urlBuilder,"bgnde"," "); /* 유기날짜(검색 시작일) (YYYYMMDD) */
+//		appendItem(urlBuilder,"endde"," "); /* 유기날짜(검색 종료일) (YYYYMMDD) */
+//		appendItem(urlBuilder,"upkind"," "); /* 축종코드 (개 : 417000, 고양이 : 422400, 기타 : 429900) */
+//		appendItem(urlBuilder,"kind"," "); /* 품종코드 (품종 조회 OPEN API 참조) */
+//		appendItem(urlBuilder,"org_cd"," "); /* 시군구코드 (시군구 조회 OPEN API 참조) */
+//		appendItem(urlBuilder,"care_reg_no"," "); /* 보호소번호 (보호소 조회 OPEN API 참조) */
+		appendItem(urlBuilder,"state","protect"); /* 상태(전체 : null(빈값), 공고중 : notice, 보호중 : protect) */
 //		appendItem(urlBuilder,"neuter_yn"," "); /* 상태 (전체 : null(빈값), 예 : Y, 아니오 : N, 미상 : U) */
 		appendItem(urlBuilder, "pageNo", "1"); // 페이지 번호 (기본값 : 1)
 		appendItem(urlBuilder, "numOfRows", "1000"); // 페이지당 보여줄 개수 (1,000 이하), 기본값 : 10
