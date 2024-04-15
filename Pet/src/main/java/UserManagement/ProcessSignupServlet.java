db044c4c375bdbe60a5843724a0f4344a9f95019
gayoung <7rmile@gmail.com>
2024-04-15 13:19:34 +0900

    최종..

--- Pet/src/main/java/UserManagement/ProcessSignupServlet.java
+++ Pet/src/main/java/UserManagement/ProcessSignupServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//@WebServlet("/SignupServlet")
+//@WebServlet("/ProcessSignupServlet")
 public class ProcessSignupServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private UserDAO userDAO;
